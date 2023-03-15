07553b5072311a2e7382c160136df04908e4cd80
Ábris Simon <abris96@gmail.com>
2023-03-15 11:52:08 +0100

    maybe fix auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import { GITHUB_ID, GITHUB_SECRET, AUTH_SECRET } from "$env/static/private"
 import GitHub from "@auth/core/providers/github"
 import { SvelteKitAuth } from "@auth/sveltekit"
 
@@ -8,4 +8,5 @@ export const handle = SvelteKitAuth({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  secret: AUTH_SECRET
 })
