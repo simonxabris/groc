b444eda09019e51325f5e30e8a9212dc288018f4
Ábris Simon <abris96@gmail.com>
2023-03-26 19:13:46 +0200

    add page for registering new shopping

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 
 <header class="w-full py-4 bg-white shadow-md dark:bg-gray-900">
 	<div class="container mx-auto">
-		<h1 class="text-4xl font-bold text-center text-blue-600 dark:text-blue-400">groc</h1>
+		<a href="/" class="block text-4xl font-bold text-center text-blue-600 dark:text-blue-400"
+			>groc</a
+		>
 	</div>
 </header>
 
---
+++ src/routes/shopping/new/+page.svelte
@@ -0,0 +1,3 @@
+<div class="dark:text-gray-300">
+	<h1>Add new shopping trip</h1>
+</div>
