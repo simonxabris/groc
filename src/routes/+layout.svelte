00f970958abfbe5ff28d68f12800a8b7d2a548d7
Ábris Simon <abris96@gmail.com>
2023-03-26 18:00:09 +0200

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
 	import '../app.css';
 </script>
 
+<header class="w-full py-4 bg-white shadow-md dark:bg-gray-900">
+	<div class="container mx-auto">
+		<h1 class="text-4xl font-bold text-center text-blue-600 dark:text-blue-400">groc</h1>
+	</div>
+</header>
+
 <slot />
