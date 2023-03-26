4c28c13d17a74f95af8b9ebdee0ad1cb1b433f54
Ábris Simon <abris96@gmail.com>
2023-03-26 19:08:28 +0200

    add "add new" button to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-gray-300 text-center grid place-items-center h-screen mx-4 ">
+	<a
+		class="px-2 w-full bg-yellow-500 hover:bg-blue-800 text-white font-bold py-2 rounded-md transition duration-200 dark:bg-blue-500 dark:hover:bg-blue-700"
+		href="/shopping/new">Add new</a
+	>
+</div>
